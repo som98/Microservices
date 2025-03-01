cc0526cf6b0cf1c8cf2edd6d4b93d934cf0ddbbe
Somshubhra Roy <somshubhraroy98@gmail.com>
2025-03-02 01:13:35 +0530

    Implemented a Retry pattern in the custom routing of Loans service in Gatewayserver application.

--- gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/GatewayserverApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -19,6 +21,7 @@ public class GatewayserverApplication {
 	public RouteLocator somBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
 
 		return routeLocatorBuilder.routes()
+
 				.route(p -> p
 						.path("/sombank/accounts/**")
 						.filters( f -> f.rewritePath("/sombank/accounts/(?<segment>.*)","/${segment}")
@@ -26,16 +29,22 @@ public class GatewayserverApplication {
 								.circuitBreaker(config -> config.setName("accountsCircuitBreaker")
 										.setFallbackUri("forward:/contactSupportTeam")))
 						.uri("lb://ACCOUNTS"))
+
 				.route(p -> p
 						.path("/sombank/loans/**")
 						.filters( f -> f.rewritePath("/sombank/loans/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> retryConfig.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(500),Duration.ofMillis(2000),2, true)))
 						.uri("lb://LOANS"))
+
 				.route(p -> p
 						.path("/sombank/cards/**")
 						.filters( f -> f.rewritePath("/sombank/cards/(?<segment>.*)","/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://CARDS"))
+
 				.build();
 	}
 
