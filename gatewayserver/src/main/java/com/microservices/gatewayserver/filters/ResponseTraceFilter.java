451292017751408ddfd470449af6918e690cfb69
Somshubhra Roy <somshubhraroy98@gmail.com>
2025-03-03 02:24:36 +0530

    Bug fix applied to set CorrelationId only one time.

--- gatewayserver/src/main/java/com/microservices/gatewayserver/filters/ResponseTraceFilter.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/filters/ResponseTraceFilter.java
@@ -21,8 +21,12 @@ public class ResponseTraceFilter {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
                 String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                log.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+
+                if (!exchange.getResponse().getHeaders().containsKey(FilterUtility.CORRELATION_ID)) {
+                    log.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                    exchange.getResponse().getHeaders().set(FilterUtility.CORRELATION_ID, correlationId);
+                }
+
             }));
         };
     }
