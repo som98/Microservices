5c890e25a8748c25edb4570f1d021156772636c0
Somshubhra Roy <somshubhraroy98@gmail.com>
2025-04-10 15:34:51 +0530

    Added the properties to implement Rabbit MQ and Message microservice to interact with Accounts microservices. Updated the Tag versions for all Microservices to v11. Changes applied in all docker-compose.yml files accordingly for all envs under the docker-compose directory.

--- cards/src/main/java/com/microservices/cards/CardsApplication.java
+++ cards/src/main/java/com/microservices/cards/CardsApplication.java
@@ -18,7 +18,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		info = @Info(
 				title = "Cards microservice REST API Documentation",
 				description = "Som Bank Cards microservice REST API Documentation",
-				version = "v10",
+				version = "v11",
 				contact = @Contact(
 						name = "Somshubhra Roy",
 						email = "sroy@gmail.com",
